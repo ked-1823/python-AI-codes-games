ab6d16e9d8031cd2e67abd81892ea4869860c940
Kedar mane <manekedar287@gmail.com>
2025-12-11 16:50:10 +0530

    Implement board display and win condition check
    Added functions to print the game board and check for a win condition.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,24 @@
+def print_board(b):
+    print('----+---+----')
+    print(f'{b[0]}   |  {b[1]}| {b[2]}')
+    print('----+---+-----')
+    print(f'{b[3]}   | {b[4]} | {b[5]}')
+    print('----+---+-----')
+    print(f'{b[6]}   | {b[7]} | {b[8]}')
+    print('----+---+-----')
+
+
+def win(b,player):
+    win_state=[
+        (0,1,2),(3,4,5),(6,7,8),
+        (0,3,6),(1,4,7),(2,5,8),
+        (0,4,8),(2,4,6)
+        ]
+    for x,y,z in win_state:
+        if b[x]==b[y]==b[z]==player:
+            return True
+    return False
+
 def tic_tac_toe():
     board=[' ']*9
     current='x'
@@ -18,3 +39,9 @@ def tic_tac_toe():
     print('its a draw')
 
 tic_tac_toe()
+        
+        
+        
+        
+        
+        
